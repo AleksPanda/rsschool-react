404ce8b06a1197c6d50996dc330c7de923bf9c65
AleksPanda <149190602+AleksPanda@users.noreply.github.com>
2026-05-29 17:57:16 +0300

    chore: add React Query Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ThemeProvider from './context/theme-provider.tsx';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './query-client.ts';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -16,6 +17,8 @@ createRoot(document.getElementById('root')!).render(
           <BrowserRouter>
             <App />
           </BrowserRouter>
+
+          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </ThemeProvider>
     </ErrorBoundary>
